e1364d0b482c7d506b4c607644a4904663026201
Decision <personal@decision01.com>
2022-06-06 08:36:28 +0800

    fixed: protobuf permission -> command

--- internal/rpc/command.go
+++ internal/rpc/command.go
@@ -1,3 +1,18 @@
 package rpc
 
-type Per
+type CommandService struct {
+}
+
+func (cs *CommandService) CommandProxy(req CommandRequest, resp *CommandRespond) error {
+
+	command := req.GetCommand()
+	subCommand := req.GetSubCommand()
+	params := req.GetParams()
+
+	switch command {
+	case "permission":
+		PermissionProxy(subCommand, &params, resp)
+	}
+
+	return nil
+}
---
+++ internal/rpc/command_permission.go
@@ -0,0 +1,22 @@
+package rpc
+
+import (
+	"google.golang.org/protobuf/types/known/structpb"
+)
+
+func PermissionProxy(subCommand string, params *map[string]*structpb.Value, resp *CommandRespond) {
+	switch subCommand {
+	case "add":
+		subPermissionAdd(params, resp)
+	case "revoke":
+		subPermissionRevoke(params, resp)
+	}
+}
+
+func subPermissionAdd(params *map[string]*structpb.Value, resp *CommandRespond) {
+
+}
+
+func subPermissionRevoke(params *map[string]*structpb.Value, resp *CommandRespond) {
+
+}
